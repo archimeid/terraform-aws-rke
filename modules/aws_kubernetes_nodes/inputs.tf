f16b4d1f45142d709125482e07f8e71a26c13110
Archimeid <68751170+archimeid@users.noreply.github.com>
2022-01-14 13:55:09 +0100

    Fix vpc and subnets

--- modules/aws_kubernetes_nodes/inputs.tf
+++ modules/aws_kubernetes_nodes/inputs.tf
@@ -10,10 +10,10 @@ variable "vpc_cidr_block" {
 
 locals {
   subnets_cidr_block = cidrsubnets(var.vpc_cidr_block, 8, 8, 8, 8)
-  public_subnet1_cidr_block = locals.subnets_cidr_block[0]
-  public_subnet2_cidr_block = locals.subnets_cidr_block[1]
-  private_subnet1_cidr_block = locals.subnets_cidr_block[2]
-  private_subnet2_cidr_block = locals.subnets_cidr_block[3]
-  az1_name = "${ var.aÒws_region }a"
+  public_subnet1_cidr_block = local.subnets_cidr_block[0]
+  public_subnet2_cidr_block = local.subnets_cidr_block[1]
+  private_subnet1_cidr_block = local.subnets_cidr_block[2]
+  private_subnet2_cidr_block = local.subnets_cidr_block[3]
+  az1_name = "${ var.aws_region }a"
   az2_name = "${ var.aws_region }b"
 }
--- modules/aws_kubernetes_nodes/network_resources.tf
+++ modules/aws_kubernetes_nodes/network_resources.tf
@@ -1,27 +1,27 @@
 resource "aws_vpc" "kubernetes" {
   cidr_block = var.vpc_cidr_block
 
-  tags {
+  tags = {
       Terraform = true
       Name = "kubernetes_vpc"
   }
 }
 
 data "aws_availability_zone" "availability_zone_a" {
-  name = locals.az1_name
+  name = local.az1_name
 }
 
 data "aws_availability_zone" "availability_zone_b" {
-  name = locals.az2_name
+  name = local.az2_name
 }
 
 resource "aws_subnet" "public_subnet_1" {
   vpc_id = aws_vpc.kubernetes.id
-  cidr_block = locals.public_subnet1_cidr_block
+  cidr_block = local.public_subnet1_cidr_block
   availability_zone = data.aws_availability_zone.availability_zone_a.id
   map_public_ip_on_launch = true
 
-  tags {
+  tags = {
     Terraform = true
     Name = "public-subnet-1"
   }
@@ -29,11 +29,11 @@ resource "aws_subnet" "public_subnet_1" {
 
 resource "aws_subnet" "public_subnet_2" {
   vpc_id = aws_vpc.kubernetes.id
-  cidr_block = locals.public_subnet2_cidr_block
+  cidr_block = local.public_subnet2_cidr_block
   availability_zone = data.aws_availability_zone.availability_zone_b.id
   map_public_ip_on_launch = true
 
-  tags {
+  tags = {
     Terraform = true
     Name = "public-subnet-2"
   }
@@ -41,11 +41,11 @@ resource "aws_subnet" "public_subnet_2" {
 
 resource "aws_subnet" "private_subnet_1" {
   vpc_id = aws_vpc.kubernetes.id
-  cidr_block = locals.private_subnet1_cidr_block
+  cidr_block = local.private_subnet1_cidr_block
   availability_zone = data.aws_availability_zone.availability_zone_a.id
   map_public_ip_on_launch = false
 
-  tags {
+  tags = {
     Terraform = true
     Name = "private-subnet-1"
   }
@@ -53,11 +53,11 @@ resource "aws_subnet" "private_subnet_1" {
 
 resource "aws_subnet" "private_subnet_2" {
   vpc_id = aws_vpc.kubernetes.id
-  cidr_block = locals.private_subnet2_cidr_block
+  cidr_block = local.private_subnet2_cidr_block
   availability_zone = data.aws_availability_zone.availability_zone_b.id
   map_public_ip_on_launch = false
 
-  tags {
+  tags = {
     Terraform = true
     Name = "private-subnet-2"
   }

f482fb24bff97e725adbb493593f51827b11dc6d
Archimeid <68751170+archimeid@users.noreply.github.com>
2022-01-19 18:13:49 +0100

    Use aws_profile in profile

--- kubernetes_cluster.tf
+++ kubernetes_cluster.tf
@@ -2,6 +2,7 @@ module "aws_kubernetes_nodes" {
   source = "./modules/aws_kubernetes_nodes"
 
   aws_region = var.aws_region
+  aws_profile = var.aws_profile
   vpc_cidr_block = var.vpc_cidr_block
   domain_name = var.domain_name
 }

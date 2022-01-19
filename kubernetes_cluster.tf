20f2cfcea25352c086080989f31c206270370e6d
Archimeid <68751170+archimeid@users.noreply.github.com>
2022-01-19 17:51:24 +0100

    Call aws_kubernetes_nodes in root

--- kubernetes_cluster.tf
+++ kubernetes_cluster.tf
@@ -1,6 +1,7 @@
 module "aws_kubernetes_nodes" {
   source = "./modules/aws_kubernetes_nodes"
 
-  aws_region = "eu-west-3"
-  vpc_cidr_block = "10.0.0.0/16"
+  aws_region = var.aws_region
+  vpc_cidr_block = var.vpc_cidr_block
+  domain_name = var.domain_name
 }

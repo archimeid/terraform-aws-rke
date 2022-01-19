b4dbff8aa759bcadca34d683d35f5585737af001
Archimeid <68751170+archimeid@users.noreply.github.com>
2022-01-19 17:54:06 +0100

    [FIX] Error Syntax: local_file -> resource

--- modules/aws_kubernetes_nodes/private_key.tf
+++ modules/aws_kubernetes_nodes/private_key.tf
@@ -8,7 +8,7 @@ resource "aws_key_pair" "k8s" {
   public_key = tls_private_key.k8s.public_key_openssh
 }
 
-local_file "local_file" "k8s-private-key" {
+resource "local_file" "k8s-private-key" {
   content = tls_private_key.k8s.private_key_pem
   filename = "${path.root}/generated/k8s_private_key"
 }

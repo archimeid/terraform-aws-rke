b3feca11311309fd4d6abf840a84f0f49410e038
Archimeid <68751170+archimeid@users.noreply.github.com>
2022-01-19 19:02:22 +0100

    Create internet gateway and add a route for it

--- modules/aws_kubernetes_nodes/network.tf
+++ modules/aws_kubernetes_nodes/network.tf
@@ -62,3 +62,27 @@ resource "aws_subnet" "private_subnet_2" {
     Name = "private-subnet-2"
   }
 }
+
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.kubernetes.id
+
+  tags = {
+    Name = "k8s-ig"
+  }
+}
+
+resource "aws_default_route_table" "kubernetes" {
+  default_route_table_id = aws_vpc.kubernetes.default_route_table_id
+
+  # Allow outgoing traffic through Internet Gateway
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  # local route is automatically created
+
+  tags = {
+    Name = "k8s-rt"
+  }
+}

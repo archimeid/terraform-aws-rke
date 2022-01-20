9cb7d4b2bc68c565b076ae3d74eaf38644eec8f0
Archimeid <68751170+archimeid@users.noreply.github.com>
2022-01-20 22:01:48 +0100

    Add x64 AMI for Amazon Linux 2

--- modules/aws_kubernetes_nodes/amazon_ami.tf
+++ modules/aws_kubernetes_nodes/amazon_ami.tf
@@ -3,5 +3,5 @@ data "aws_ssm_parameter" "amazon_linux_arm_ami" {
 }
 
 data "aws_ssm_parameter" "amazon_linux_ami" {
-  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-arm64-gp2"
+  name = "/aws/service/ami-amazon-linux-latest/amzn2-ami-hvm-x86_64-gp2"
 }
